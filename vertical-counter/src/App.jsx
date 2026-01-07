20d081b36c64c2979a07eec0327a7e85f81598dc
yashgarg30 <y.g.639sm@gmail.com>
2026-01-07 11:00:21 +0530

    Add vertical-counter folder

---
+++ vertical-counter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">
+      <h1>COUNTER SPA</h1>
+      <p>Count: {count}</p>
+
+      <div className="btn-group">
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
